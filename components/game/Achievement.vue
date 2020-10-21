641be5f8ef526c5b981ee29283f8d8b2dfbdf08c
Nishinoyama <begoingto1414@gmail.com>
2020-10-21 21:08:20 +0900

    コンポーネント実装：実績ページ

--- components/game/Achievement.vue
+++ components/game/Achievement.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="achievement">
-    <p>
-      実績
-    </p>
+    <div v-for="achieve in achievements" :key="achieve" class="achievementBox">
+      <p> {{ achieve }} </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Achievement'
+  name: 'Achievement',
+  data () {
+    return {
+      achievements: []
+    }
+  },
+  created () {
+    for (let i = 0; i < 500; i++) {
+      this.achievements.push(`実績${i}`)
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.achievement{
+  text-align: left;
+  width: 100%;
+  max-width: 840px;
+  margin: 0 auto;
+  overflow-y: scroll;
+  padding: 12px;
+}
+.achievementBox{
+  display: inline-block;
+  box-sizing: border-box;
+  width: 60px;
+  height: 60px;
+  margin: 1px;
+  word-wrap: normal;
+  border: #2d2219 solid 1px;
+  // background: #544330;
+}
 </style>
