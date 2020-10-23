6829845bd2534ea057425b640eebd4630598c5b1
jellyfish26 <jellyfish0162@gmail.com>
2020-10-24 02:49:41 +0900

    Integrated Menubar into the deafult

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,95 @@
 <template>
   <div>
-    <Nuxt />
+    <nuxt />
+    <div class="menu-bar">
+      <div
+        v-for="(menu) in gameMenus"
+        :key="menu.name"
+        class="menu-item"
+      >
+        <div class="menu-text">
+          {{ menu.name }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang='scss'>
+<script>
+export default {
+  data () {
+    return {
+      gameMenus: [
+        {
+          name: 'クリック',
+          component: 'Click'
+        },
+        {
+          name: '施設',
+          component: 'Building'
+        },
+        {
+          name: '状態',
+          component: 'Statement'
+        },
+        {
+          name: '実績',
+          component: 'Achievement'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+$menu-border:1px solid black;
+
+.menu-bar{
+  display: block;
+  height: min(10vh, 80px);
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  background: #999999;
+  text-align: center;
+  font-size: 14px;
+
+  @include tab {
+    font-size: 16px;
+  }
+
+  @include sp {
+    font-size: 18px;
+  }
+}
+
+.menu-item{
+  display: inline-block;
+  width: 25%;
+  max-width: 200px;
+  height: 100%;
+  position: relative;
+  border-left: $menu-border;
+  &:last-child{
+    border-right: $menu-border;
+  }
+
+  & .menu-text{
+    @include center;
+  }
+
+  &.menu-pic{
+    background: #6b6b6b;
+  }
+
+  &:hover{
+    cursor: pointer;
+    background: #c0c0c0;
+  }
+
+}
+
 .body-class {
   margin: 0;
   overflow: hidden;
