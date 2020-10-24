60a76a035d2197181b18740f2141a6f4fd590231
Nishinoyama <begoingto1414@gmail.com>
2020-10-24 23:39:04 +0900

    追加：状態ページ、暫定的なAPIの叩き方

---
+++ components/Statement.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="section">
+    <h1 class="section-title">
+      {{ title }}
+    </h1>
+    <div v-for="statement in statements" :key="statement.name" class="subsection">
+      <h2 class="subsection-title">
+        {{ statement.subtitle }}
+      </h2>
+      <p class="statement-number">
+        {{ statement.value }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Statement',
+  props: {
+    title: {
+      type: String,
+      default: 'セクションタイトル'
+    },
+    statements: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.section{
+  text-align: center;
+  h1, h2 {
+    text-align: left;
+  }
+}
+</style>
--- pages/statement.vue
+++ pages/statement.vue
@@ -1,19 +1,88 @@
 <template>
   <div class="statement">
-    <p>
-      状態
-    </p>
+    <div class="section">
+      <Statement :statements="statements.general" :title="'全体'" class="subsection" />
+      <Statement :statements="statements.click" :title="'クリック関連'" class="subsection" />
+    </div>
   </div>
 </template>
 
 <script>
+import Statement from '@/components/Statement'
 export default {
+  components: { Statement },
+  data () {
+    return {
+      statements: {
+        general: [
+          // order は配列の順番を指定する
+          // バックエンドで配列の順番を決められるなら order は消す
+          {
+            order: 0,
+            name: 'totalCount',
+            subtitle: 'これまでの鹿せんべい生産量',
+            value: 0
+          },
+          {
+            order: 10,
+            name: 'nowCount',
+            subtitle: '現在の鹿せんべい所持数  ',
+            value: 0
+          },
+          {
+            order: 20,
+            name: 'totalUsers',
+            subtitle: 'これまでの来訪者数',
+            value: 0
+          },
+          {
+            order: 30,
+            name: 'nowUsers',
+            subtitle: '現在の来訪者数',
+            value: 0
+          }
+        ],
+        click: [
+          {
+            order: 0,
+            name: 'totalClick',
+            subtitle: 'これまでのクリック数',
+            value: 0
+          },
+          {
+            order: 10,
+            name: 'totalClickGen',
+            subtitle: 'これまでのクリックによる生産数',
+            value: 0
+          }
+        ]
+      }
+    }
+  },
+  created () {
+    this.loadStatement()
+  },
+  methods: {
+    async loadStatement () {
+      await this.$axios.$get('/api/statement', {
+        responseType: 'json'
+      })
+        .then((response) => {
+          this.statement = response
+        })
+        .catch((error) => {
+          // エラー表示を無理やり
+          // TODO何かしらで書き換える
+          this.statement.general[0].value = error
+        })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .statement{
-  text-align: center;
+  overflow-y: auto;
 }
 
 </style>
