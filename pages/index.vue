f9273b4be708de0bfa193dabc3e7ed9a72189ee6
akatsuki1910 <akatsukicirno@gmail.com>
2020-10-25 16:31:40 +0900

    Click animation
    クリックした際のアニメーション

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="click">
-    <img class="big-click" src="@/assets/bigClick.png" alt="bigClick" @click="click">
+    <img :class="['big-click', {'big-click-fire' : bcf}]" src="@/assets/bigClick.png" alt="bigClick" @click="click">
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      bcf: false
+    }
+  },
   methods: {
     click () {
+      this.bcf = false
+      requestAnimationFrame(() => {
+        this.bcf = true
+        console.log(this.bcf)
+      })
+      console.log(this.bcf)
       this.$store.dispatch('clicker/increment', 1)
     }
   }
@@ -19,5 +30,20 @@ export default {
   @include center;
   height: 288px;
   width: 288px;
+}
+
+.big-click-fire{
+  animation: anime .5s linear;
+}
+
+@keyframes anime {
+  0% {transform: translate(-50%,-50%) scale(1);}
+  10% {transform: translate(-50%,-50%) scale(.7);}
+  20% {transform: translate(-50%,-50%) scale(1);}
+  30% {transform: translate(-50%,-50%) scale(.9);}
+  70% {transform: translate(-50%,-50%) scale(1.1);}
+  80% {transform: translate(-50%,-50%) scale(1);}
+  90% {transform: translate(-50%,-50%) scale(1.3);}
+  100% {transform: translate(-50%,-50%) scale(1);}
 }
 </style>
