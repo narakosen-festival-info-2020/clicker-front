c46b6e3ac6bbbbe824f90c279968dad0d8d3fb69
akatsuki1910 <akatsukicirno@gmail.com>
2020-10-26 22:40:10 +0900

    Cookie animation
    とりあえずふわふわする

--- pages/index.vue
+++ pages/index.vue
@@ -33,61 +33,6 @@ export default {
 }
 
 .big-click-fire{
-  animation: anime .5s linear;
-}
-
-@function fuyofuyo($i){
-  @if $i < 0.5 {
-    @return 0.4 * pow(2*$i, 5) * sin(44 * $i,true) + 1;
-  } @else {
-    @return 0.4 * pow(2 - 2*$i, 5) * sin(44 * $i,true) + 1;
-  }
-}
-
-@function pow($base, $exp){
-  $value: $base;
-  @if $exp >= 1 {
-    @for $i from 1 through $exp {
-      $value: $value * $base;
-    }
-  } @else if $exp <= -1 {
-    @for $i from 1 through -$exp {
-      $value: $value / $base;
-    }
-  } @else {
-    $value: 1;
-  }
-  @return $value;
-}
-
-@function fact($val){
-  $value: 1;
-  @if $val > 0{
-    @for $i from 1 through $val{
-      $value: $value * $i;
-    }
-  } @else {
-    $value: 1;
-  }
-  @return $value;
-}
-
-$pi: 3.14159265359; // 円周率
-$iter: 30;
-@function sin($angle, $degrees: false) {
-  $sin: 0;
-  @if $degrees {
-    $angle: $angle / 180 * $pi;
-  }
-  @for $n from 0 through $iter - 1 {
-    $sin: $sin + (pow(-1, $n) / fact(2*$n + 1)) * pow($angle, 2*$n + 1);
-  }
-  @return $sin;
-}
-
-@keyframes anime {
-  @for $i from 0 through 100 {
-    #{$i}% {transform: translate(-50%,-50%) scale(fuyofuyo($i / 100))};
-  }
+  animation: anime 1s linear;
 }
 </style>
