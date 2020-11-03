3b724a30402c0b46ee76b83d0c12fe975c144504
Nishinoyama <begoingto1414@gmail.com>
2020-11-03 22:29:07 +0900

    追加：静的404ページ

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="error">
+    <div class="message">
+      <p>
+        <strong>
+          Error has occurred
+        </strong>
+      </p>
+    </div>
+    <nuxt-link to="/">
+      クリックページへ戻る
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.error{
+  text-align: center;
+  margin: auto;
+}
+</style>
